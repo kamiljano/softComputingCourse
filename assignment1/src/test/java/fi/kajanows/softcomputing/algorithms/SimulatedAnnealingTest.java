0f0d8532ec67b2423c45f5b2021fa6c67116eae3
kamiljano <kamiljano@yahoo.pl>
2015-11-08 11:10:28 +0200

    added timeouts

--- assignment1/src/test/java/fi/kajanows/softcomputing/algorithms/SimulatedAnnealingTest.java
+++ assignment1/src/test/java/fi/kajanows/softcomputing/algorithms/SimulatedAnnealingTest.java
@@ -15,7 +15,7 @@ public class SimulatedAnnealingTest {
     private static final double COOLING_RATE = 0.999;
     private static final double STARTING_TEMP = 1000;
 
-    @Test
+    @Test(timeout = 10000)
     public void testFindOptimum_simple() throws Exception {
         SimulatedAnnealing annealing = new SimulatedAnnealing(COOLING_RATE, STARTING_TEMP, -2, 2);
         Point2D optimum = annealing.findOptimum(x -> -(x * x) + 1);
@@ -23,7 +23,7 @@ public class SimulatedAnnealingTest {
         assertEquals(0, optimum.getX(), 0.01);
     }
 
-    @Test
+    @Test(timeout = 10000)
     public void testFindOptimum_complex() throws Exception {
         SimulatedAnnealing annealing = new SimulatedAnnealing(COOLING_RATE, STARTING_TEMP, -2, 2);
         Point2D optimum = annealing.findOptimum(x -> Math.sin(x) / x);
@@ -31,7 +31,7 @@ public class SimulatedAnnealingTest {
         assertEquals(0, optimum.getX(), 0.01);
     }
 
-    @Test
+    @Test(timeout = 10000)
     public void testFindOptimum_complex2() throws Exception {
         SimulatedAnnealing annealing = new SimulatedAnnealing(COOLING_RATE, STARTING_TEMP, -2, 2);
         Point2D optimum = annealing.findOptimum(x -> Math.sin(1 + x) / (2 * (1 + x)));
@@ -39,7 +39,7 @@ public class SimulatedAnnealingTest {
         assertEquals(-1, optimum.getX(), 0.01);
     }
 
-    @Test
+    @Test(timeout = 10000)
     public void testFindOptimum_3d() throws Exception {
         SimulatedAnnealing annealing = new SimulatedAnnealing(COOLING_RATE, STARTING_TEMP, -2, 2);
         Point3D optimum = annealing.findOptimum((x, y) -> -(x * x) - (y * y) + 1);
@@ -48,13 +48,13 @@ public class SimulatedAnnealingTest {
         assertEquals(1, optimum.getY(), 0.01);
     }
 
-    @Test
+    @Test(timeout = 10000)
     public void testFindOptimum_3d_actualHomeworkFunction() throws Exception {
         SimulatedAnnealing annealing = new SimulatedAnnealing(COOLING_RATE, 10000000, 0, 5);
         Point3D optimum = annealing.findOptimum((x, y) -> Math.pow((x * x) + y - 11, 2) + Math.pow((y * y) + x - 7, 2), new Point2D(2.5, 2.5));
         assertEquals(5, optimum.getX(), 0.01);
         assertEquals(5, optimum.getZ(), 0.01);
-        assertEquals(890, optimum.getY(), 1);
+        assertEquals(890, optimum.getY(), 2);
     }
 
 }
---
+++ assignment2/src/main/java/fi/kajanows/softcomputing/DifferentialEvolution.java
@@ -0,0 +1,7 @@
+package fi.kajanows.softcomputing;
+
+/**
+ * Created by kgjan on 8.11.2015.
+ */
+public class DifferentialEvolution {
+}
